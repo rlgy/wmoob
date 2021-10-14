e96a73974f7b04b2fba5ac3dae6ecb1da4a661e8
rlgyzhcn <rlgyzhcn@qq.com>
2021-10-14 16:07:47 +0800

    first commit

---
+++ src/Wmoob.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: rlgyzhcn
+ * Mail <rlgyzhcn@gmail.com>
+ * Date: 2021/10/14 15:58
+ */
+
+
+namespace Wmoob;
+
+/**
+ * weimob cloud php skd
+ */
+class Wmoob
+{
+    const LIBRARY_VERSION = '1.0.0';
+}
